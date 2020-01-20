f33aca1481968792dbe347893865a8ffaa764139
Alan Boglioli <alan.boglioli@gmail.com>
2020-01-20 03:31:40 -0300

    add soem tests to Postgres connection

--- internal/users/repository_test.go
+++ internal/users/repository_test.go
@@ -3,28 +3,95 @@ package users
 import (
 	"database/sql"
 	"testing"
-	"time"
 
+	"github.com/aboglioli/big-brother/pkg/config"
+	"github.com/aboglioli/big-brother/pkg/db"
+	"github.com/aboglioli/big-brother/pkg/errors"
 	"github.com/aboglioli/big-brother/pkg/models"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func populate(db *sql.DB) error {
-	if _, err := db.Exec(`
-		INSERT INTO users(id, username, password, email, name, lastname, role, validated, enabled, created_at)
-			VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
-	`, models.NewID(), "user1", "hashed.password.1", "user1@user.com", "User", "One", "user", true, true, time.Now()); err != nil {
-		return err
+func populate(db *sql.DB, users ...*models.User) error {
+
+	for _, user := range users {
+		_, err := db.Exec(`
+			INSERT INTO users(id, username, password, email, name, lastname, role, validated, enabled, created_at)
+				VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+		`, user.ID, user.Username, user.Password, user.Email, user.Name, user.Lastname, user.Role, user.Validated, user.Enabled, user.CreatedAt)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
 }
 
 func TestFindByID(t *testing.T) {
-	// c := config.Get()
-	// db, err := db.ConnectPostgres(c.PostgresURL, "users_and_organizations", c.PostgresUsername, c.PostgresPassword)
-	// require.Nil(t, err)
-	// require.NotNil(t, db)
+	c := config.Get()
+	db, err := db.ConnectPostgres(c.PostgresURL, "test", c.PostgresUsername, c.PostgresPassword)
+	require.Nil(t, err)
+	require.NotNil(t, db)
 
-	// err = populate(db)
-	// require.Nil(t, err)
+	user1 := models.NewUser()
+	user1.Username = "user1"
+	user1.Password = "hashed.password.1"
+	user1.Email = "user1@user.com"
+	user1.Name = "First"
+	user1.Lastname = "User"
+	user2 := models.NewUser()
+	user2.Username = "user2"
+	user2.Password = "hashed.password.2"
+	user2.Email = "user2@user.com"
+	user2.Name = "Second"
+	user2.Lastname = "User"
+	user2.Validated = true
+	user3 := models.NewUser()
+	user3.Username = "user3"
+	user3.Password = "hashed.password.3"
+	user3.Email = "user3@user.com"
+	user3.Name = "Third"
+	user3.Lastname = "User"
+	user3.Validated = true
+	user3.Enabled = false
+
+	err = populate(db)
+	require.Nil(t, err)
+
+	tests := []struct {
+		name string
+		id   string
+		err  error
+		user *models.User
+	}{{
+		"invalid id",
+		"user123",
+		ErrRepositoryNotFound,
+		nil,
+	}, {
+		"non existing",
+		"ed84840c-6e9c-46c4-9409-86283a9fa961",
+		ErrRepositoryNotFound,
+		nil,
+	}, {
+		"existing",
+		user2.ID,
+		nil,
+		user2.Clone(),
+	}, {
+		"another",
+		user3.ID,
+		nil,
+		user3.Clone(),
+	}}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert := assert.New(t)
+			repo := NewRepository()
+			user, err := repo.FindByID(test.id)
+			assert.Equal(test.user, user)
+			errors.Assert(t, test.err, err)
+		})
+	}
 }
--- pkg/db/postgres_test.go
+++ pkg/db/postgres_test.go
@@ -14,15 +14,14 @@ func TestPostgres(t *testing.T) {
 	if testing.Short() {
 		t.Skip()
 	}
-	assert := assert.New(t)
 
 	db, err := ConnectPostgres("invalid", "invalid", "invalid", "invalid")
-	assert.NotNil(err)
-	assert.Nil(db)
+	assert.NotNil(t, err)
+	assert.Nil(t, db)
 
 	c := config.Get()
 	db, err = ConnectPostgres(c.PostgresURL, "test", c.PostgresUsername, c.PostgresPassword)
-	if !assert.Nil(err) {
+	if !assert.Nil(t, err) {
 		t.Log(err.(errors.Error).Cause)
 	}
 	require.NotNil(t, db)
@@ -35,7 +34,7 @@ func TestPostgres(t *testing.T) {
 				lastname text
 			);
 		`)
-	assert.Nil(err)
+	assert.Nil(t, err)
 
 	_, err = db.Exec(`
 		INSERT INTO test_table(name, age, lastname)
@@ -43,14 +42,7 @@ func TestPostgres(t *testing.T) {
 		INSERT INTO test_Table(name, age, lastname)
 			VALUES('Boglioli', 23, 'Caffe');
 	`)
-	assert.Nil(err)
-
-	row := db.QueryRow("SELECT COUNT(*) FROM test_table")
-	assert.NotNil(row)
-	var count int
-	err = row.Scan(&count)
-	assert.Nil(err)
-	assert.Equal(2, count)
+	assert.Nil(t, err)
 
 	type person struct {
 		name     string
@@ -58,31 +50,87 @@ func TestPostgres(t *testing.T) {
 		lastname *string
 	}
 
-	rows, err := db.Query("SELECT * FROM test_table")
-	assert.Nil(err)
-	people := make([]person, 0)
-	for rows.Next() {
-		var p person
-		err := rows.Scan(&p.name, &p.age, &p.lastname)
+	t.Run("count", func(t *testing.T) {
+		assert := assert.New(t)
+		row := db.QueryRow("SELECT COUNT(*) FROM test_table")
+		assert.NotNil(row)
+		var count int
+		err = row.Scan(&count)
 		assert.Nil(err)
-		people = append(people, p)
-	}
-	rows.Close()
-	assert.Nil(rows.Err())
-	assert.Len(people, 2)
-	assert.Equal(person{"Alan", 21, nil}, people[0])
-	assert.Equal(person{"Boglioli", 23, utils.NewString("Caffe")}, people[1])
+		assert.Equal(2, count)
+	})
 
-	row = db.QueryRow("SELECT * FROM test_table WHERE lastname = $1 and age > $2", "Caffe", 20)
-	var p person
-	err = row.Scan(&p.name, &p.age, &p.lastname)
-	assert.Nil(err)
-	assert.Equal(person{"Boglioli", 23, utils.NewString("Caffe")}, p)
+	t.Run("multiple rows", func(t *testing.T) {
+		assert := assert.New(t)
+		rows, err := db.Query("SELECT * FROM test_table")
+		defer rows.Close()
+		assert.Nil(err)
+		people := make([]person, 0)
+		for rows.Next() {
+			var p person
+			err := rows.Scan(&p.name, &p.age, &p.lastname)
+			assert.Nil(err)
+			people = append(people, p)
+		}
+		assert.Nil(rows.Err())
+		assert.Len(people, 2)
+		assert.Equal(person{"Alan", 21, nil}, people[0])
+		assert.Equal(person{"Boglioli", 23, utils.NewString("Caffe")}, people[1])
+	})
 
-	row = db.QueryRow("SELECT * FROM test_table WHERE lastname = $1 and age < $2", "Caffe", 20)
-	err = row.Scan(&p.name, &p.age, &p.lastname)
-	assert.NotNil(err)
+	t.Run("query single row", func(t *testing.T) {
+		assert := assert.New(t)
+		row := db.QueryRow("SELECT * FROM test_table WHERE lastname = $1 and age > $2", "Caffe", 20)
+		var p person
+		err = row.Scan(&p.name, &p.age, &p.lastname)
+		assert.Nil(err)
+		assert.Equal(person{"Boglioli", 23, utils.NewString("Caffe")}, p)
+
+		row = db.QueryRow("SELECT * FROM test_table WHERE lastname = $1 and age < $2", "Caffe", 20)
+		err = row.Scan(&p.name, &p.age, &p.lastname)
+		assert.NotNil(err)
+	})
+
+	t.Run("nil field", func(t *testing.T) {
+		assert := assert.New(t)
+		p := person{name: "name1", age: 25, lastname: utils.NewString("lastname1")}
+		_, err = db.Exec("INSERT INTO test_table(name, age, lastname) VALUES($1, $2, $3)", &p.name, &p.age, &p.lastname)
+		assert.Nil(err)
+
+		p = person{}
+		row := db.QueryRow("SELECT * FROM test_table WHERE name = 'name1'")
+		err = row.Scan(&p.name, &p.age, &p.lastname)
+		assert.Nil(err)
+		assert.Equal(person{name: "name1", age: 25, lastname: utils.NewString("lastname1")}, p)
+
+		p = person{name: "name2", age: 26, lastname: nil}
+		_, err = db.Exec("INSERT INTO test_table(name, age, lastname) VALUES($1, $2, $3)", &p.name, &p.age, &p.lastname)
+		assert.Nil(err)
+
+		p = person{}
+		row = db.QueryRow("SELECT * FROM test_table WHERE name = 'name2'")
+		err = row.Scan(&p.name, &p.age, &p.lastname)
+		assert.Nil(err)
+		assert.Equal(person{name: "name2", age: 26, lastname: nil}, p)
+	})
+
+	t.Run("transaction rollback", func(t *testing.T) {
+		assert := assert.New(t)
+		tx, err := db.Begin()
+		assert.Nil(err)
+		assert.NotNil(tx)
+
+		_, err = tx.Exec("INSERT INTO test_table(name, age, lastname) VALUES('Trans', 1, 'Action')")
+		assert.Nil(err)
+		err = tx.Rollback()
+		assert.Nil(err)
+
+		var p person
+		row := db.QueryRow("SELECT * FROM test_table WHERE name = 'Trans' AND lastname = 'Action'")
+		err = row.Scan(&p.name, &p.age, &p.lastname)
+		assert.NotNil(err)
+	})
 
 	_, err = db.Exec("DROP TABLE test_table")
-	assert.Nil(err)
+	assert.Nil(t, err)
 }
