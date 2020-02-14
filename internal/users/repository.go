1e910c8d768fe84ef47092ad58cd4b3fef3d3813
Alan Boglioli <alan.boglioli@gmail.com>
2020-02-14 20:03:57 -0300

    Implement Repository for users

--- internal/users/repository.go
+++ internal/users/repository.go
@@ -2,6 +2,8 @@ package users
 
 import (
 	"database/sql"
+	"fmt"
+	"time"
 
 	"github.com/aboglioli/big-brother/pkg/errors"
 	"github.com/aboglioli/big-brother/pkg/models"
@@ -13,6 +15,10 @@ var (
 	ErrRepositoryInsert   = errors.Internal.New("user.repository.insert")
 	ErrRepositoryUpdate   = errors.Internal.New("user.repository.update")
 	ErrRepositoryDelete   = errors.Internal.New("user.repository.delete")
+)
+
+const (
+	userFields = "id, username, password, email, name, lastname, role, validated, enabled, created_at, updated_at, deleted_at"
 )
 
 // Interfaces
@@ -37,11 +43,87 @@ func NewRepository(db *sql.DB) Repository {
 }
 
 func (r *sqlRepository) FindByID(id string) (*models.User, error) {
-	row := r.db.QueryRow(`
-		SELECT id, username, password, email, name, lastname, role, validated, enabled, created_at, updated_at, deleted_at
-		FROM users
+	return r.findOne(fmt.Sprintf("SELECT %s FROM users WHERE id = $1 AND enabled = true", userFields), id)
+}
+
+func (r *sqlRepository) FindByUsername(username string) (*models.User, error) {
+	return r.findOne(fmt.Sprintf("SELECT %s FROM users WHERE username = $1 AND enabled = true", userFields), username)
+}
+
+func (r *sqlRepository) FindByEmail(email string) (*models.User, error) {
+	return r.findOne(fmt.Sprintf("SELECT %s FROM users WHERE email = $1 AND enabled = true", userFields), email)
+}
+
+func (r *sqlRepository) Insert(u *models.User) error {
+	u.CreatedAt = time.Now().UTC()
+
+	sql := fmt.Sprintf(`
+		INSERT INTO users(%s)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+	`, userFields)
+	res, err := r.db.Exec(
+		sql,
+		u.ID,
+		u.Username,
+		u.Password,
+		u.Email,
+		u.Name,
+		u.Lastname,
+		u.Role,
+		u.Validated,
+		u.Enabled,
+		u.CreatedAt,
+		u.UpdatedAt,
+		u.DeletedAt,
+	)
+	if err != nil {
+		return ErrRepositoryInsert.Wrap(err)
+	}
+	if c, err := res.RowsAffected(); c == 0 || err != nil {
+		return ErrRepositoryInsert
+	}
+
+	return nil
+}
+
+func (r *sqlRepository) Update(u *models.User) error {
+	u.UpdatedAt = &time.Time{}
+	*u.UpdatedAt = time.Now().UTC()
+
+	res, err := r.db.Exec(`
+		UPDATE users
+		SET username = $2,
+		password = $3,
+		email = $4,
+		name = $5,
+		lastname = $6,
+		role = $7,
+		validated = $8,
+		updated_at = $9
 		WHERE id = $1
-	`, id)
+	`, u.ID, u.Username, u.Password, u.Email, u.Name, u.Lastname, u.Role, u.Validated, u.UpdatedAt)
+	if err != nil {
+		return ErrRepositoryUpdate.Wrap(err)
+	}
+	if c, err := res.RowsAffected(); c == 0 || err != nil {
+		return ErrRepositoryUpdate
+	}
+	return nil
+}
+
+func (r *sqlRepository) Delete(id string) error {
+	res, err := r.db.Exec("UPDATE users SET enabled = false, deleted_at = $2 WHERE id = $1", id, time.Now().UTC())
+	if err != nil {
+		return ErrRepositoryDelete.Wrap(err)
+	}
+	if c, err := res.RowsAffected(); c == 0 || err != nil {
+		return ErrRepositoryDelete
+	}
+	return nil
+}
+
+func (r *sqlRepository) findOne(sql string, args ...interface{}) (*models.User, error) {
+	row := r.db.QueryRow(sql, args...)
 
 	var u models.User
 	err := row.Scan(
@@ -63,24 +145,4 @@ func (r *sqlRepository) FindByID(id string) (*models.User, error) {
 	}
 
 	return &u, nil
-}
-
-func (r *sqlRepository) FindByUsername(username string) (*models.User, error) {
-	return nil, nil
-}
-
-func (r *sqlRepository) FindByEmail(email string) (*models.User, error) {
-	return nil, nil
-}
-
-func (r *sqlRepository) Insert(u *models.User) error {
-	return nil
-}
-
-func (r *sqlRepository) Update(u *models.User) error {
-	return nil
-}
-
-func (r *sqlRepository) Delete(id string) error {
-	return nil
 }
--- internal/users/repository_test.go
+++ internal/users/repository_test.go
@@ -3,40 +3,22 @@ package users
 import (
 	"database/sql"
 	"testing"
+	"time"
 
 	"github.com/aboglioli/big-brother/pkg/config"
 	"github.com/aboglioli/big-brother/pkg/db"
 	"github.com/aboglioli/big-brother/pkg/errors"
 	"github.com/aboglioli/big-brother/pkg/models"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-func populate(db *sql.DB, users ...*models.User) error {
-	for _, user := range users {
-		_, err := db.Exec(`
-			INSERT INTO users(id, username, password, email, name, lastname, role, validated, enabled, created_at)
-				VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
-		`, user.ID, user.Username, user.Password, user.Email, user.Name, user.Lastname, user.Role, user.Validated, user.Enabled, user.CreatedAt)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func TestRepositoryFindByID(t *testing.T) {
-	if testing.Short() {
-		t.Skip()
-	}
+func populate() (*sql.DB, []*models.User) {
 	c := config.Get()
 	db, err := db.ConnectPostgres(c.PostgresURL, "test", c.PostgresUsername, c.PostgresPassword)
-	require.Nil(t, err)
-	require.NotNil(t, db)
-	defer func() {
-		db.Exec("DELETE FROM users")
-	}()
+	db.Exec("DELETE FROM users")
+	if err != nil {
+		panic(err)
+	}
 
 	user1 := models.NewUser()
 	user1.Username = "user1"
@@ -59,9 +41,27 @@ func TestRepositoryFindByID(t *testing.T) {
 	user3.Lastname = "User"
 	user3.Validated = true
 	user3.Enabled = false
+	users := []*models.User{user1, user2, user3}
 
-	err = populate(db, user1, user2, user3)
-	require.Nil(t, err)
+	for _, user := range users {
+		_, err := db.Exec(`
+			INSERT INTO users(id, username, password, email, name, lastname, role, validated, enabled, created_at)
+				VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+		`, user.ID, user.Username, user.Password, user.Email, user.Name, user.Lastname, user.Role, user.Validated, user.Enabled, user.CreatedAt)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return db, users
+}
+
+func TestRepositoryFindByID(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	db, users := populate()
+	defer db.Close()
 
 	tests := []struct {
 		name string
@@ -79,15 +79,20 @@ func TestRepositoryFindByID(t *testing.T) {
 		ErrRepositoryNotFound,
 		nil,
 	}, {
-		"existing",
-		user2.ID,
+		"not enabled",
+		users[2].ID,
+		ErrRepositoryNotFound,
 		nil,
-		user2.Clone(),
+	}, {
+		"existing",
+		users[0].ID,
+		nil,
+		users[0],
 	}, {
 		"another",
-		user3.ID,
+		users[1].ID,
 		nil,
-		user3.Clone(),
+		users[1],
 	}}
 
 	for _, test := range tests {
@@ -115,6 +120,308 @@ func TestRepositoryFindByID(t *testing.T) {
 				assert.Equal(test.user.Validated, user.Validated)
 				assert.Equal(test.user.Enabled, user.Enabled)
 				assert.Equal(test.user.CreatedAt.Format("2006-01-02T15:04:05-0700"), user.CreatedAt.Format("2006-01-02T15:04:05-0700"))
+			}
+		})
+	}
+}
+
+func TestRepositoryFindByUsername(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	db, users := populate()
+	defer db.Close()
+
+	tests := []struct {
+		name     string
+		username string
+		err      error
+		user     *models.User
+	}{{
+		"empty username",
+		"",
+		ErrRepositoryNotFound,
+		nil,
+	}, {
+		"short username",
+		"u",
+		ErrRepositoryNotFound,
+		nil,
+	}, {
+		"not enabled",
+		users[2].Username,
+		ErrRepositoryNotFound,
+		nil,
+	}, {
+		"existing user",
+		users[0].Username,
+		nil,
+		users[0],
+	}, {
+		"existing user",
+		users[1].Username,
+		nil,
+		users[1],
+	}}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert := assert.New(t)
+			r := NewRepository(db)
+
+			user, err := r.FindByUsername(test.username)
+
+			if test.err != nil {
+				errors.Assert(t, test.err, err)
+				assert.Nil(user)
+			} else {
+				assert.Nil(err)
+				if assert.NotNil(user) {
+					assert.Equal(test.user.ID, user.ID)
+					assert.Equal(test.user.Username, user.Username)
+					assert.Equal(test.user.Password, user.Password)
+					assert.Equal(test.user.Email, user.Email)
+					assert.Equal(test.user.Name, user.Name)
+					assert.Equal(test.user.Lastname, user.Lastname)
+					assert.Equal(test.user.Role, user.Role)
+					assert.Equal(test.user.Validated, user.Validated)
+					assert.Equal(test.user.Enabled, user.Enabled)
+					assert.Equal(test.user.CreatedAt.Format("2006-01-02T15:04:05-0700"), user.CreatedAt.Format("2006-01-02T15:04:05-0700"))
+				}
+			}
+		})
+	}
+}
+
+func TestRepositoryFindByEmail(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	db, users := populate()
+	defer db.Close()
+
+	tests := []struct {
+		name  string
+		email string
+		err   error
+		user  *models.User
+	}{{
+		"empty email",
+		"",
+		ErrRepositoryNotFound,
+		nil,
+	}, {
+		"invalid email",
+		"q.com",
+		ErrRepositoryNotFound,
+		nil,
+	}, {
+		"short email",
+		"u@u.com",
+		ErrRepositoryNotFound,
+		nil,
+	}, {
+		"not enabled",
+		users[2].Email,
+		ErrRepositoryNotFound,
+		nil,
+	}, {
+		"existing user",
+		users[0].Email,
+		nil,
+		users[0],
+	}, {
+		"existing user",
+		users[1].Email,
+		nil,
+		users[1],
+	}}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert := assert.New(t)
+			r := NewRepository(db)
+
+			user, err := r.FindByEmail(test.email)
+
+			if test.err != nil {
+				errors.Assert(t, test.err, err)
+				assert.Nil(user)
+			} else {
+				assert.Nil(err)
+				if assert.NotNil(user) {
+					assert.Equal(test.user.ID, user.ID)
+					assert.Equal(test.user.Username, user.Username)
+					assert.Equal(test.user.Password, user.Password)
+					assert.Equal(test.user.Email, user.Email)
+					assert.Equal(test.user.Name, user.Name)
+					assert.Equal(test.user.Lastname, user.Lastname)
+					assert.Equal(test.user.Role, user.Role)
+					assert.Equal(test.user.Validated, user.Validated)
+					assert.Equal(test.user.Enabled, user.Enabled)
+					assert.Equal(test.user.CreatedAt.Format("2006-01-02T15:04:05-0700"), user.CreatedAt.Format("2006-01-02T15:04:05-0700"))
+				}
+			}
+		})
+	}
+}
+
+func TestRepositoryInsert(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	db, users := populate()
+	defer db.Close()
+
+	user4 := models.NewUser()
+	user4.Username = "user4"
+	user4.Password = "hashed.password.4"
+	user4.Email = "user4@user.com"
+	user4.Name = "Fourth"
+	user4.Lastname = "User"
+
+	tests := []struct {
+		name string
+		user *models.User
+		err  error
+	}{{
+		"empty user",
+		models.NewUser(),
+		nil,
+	}, {
+		"existing user",
+		users[0],
+		ErrRepositoryInsert,
+	}, {
+		"valid user",
+		user4,
+		nil,
+	}}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert := assert.New(t)
+			r := NewRepository(db)
+
+			err := r.Insert(test.user)
+
+			if test.err != nil {
+				errors.Assert(t, test.err, err)
+			} else {
+				row := db.QueryRow("SELECT id, username, password, email, created_at FROM users WHERE id = $1", test.user.ID)
+				var id, username, password, email string
+				var createdAt time.Time
+				row.Scan(&id, &username, &password, &email, &createdAt)
+				assert.Equal(test.user.ID, id)
+				assert.Equal(test.user.Username, username)
+				assert.Equal(test.user.Password, password)
+				assert.Equal(test.user.Email, email)
+				assert.Equal(test.user.CreatedAt.Format("2006-01-02T15:04:05-0700"), createdAt.Format("2006-01-02T15:04:05-0700"))
+			}
+		})
+	}
+}
+
+func TestRepositoryUpdate(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	db, users := populate()
+	defer db.Close()
+
+	user4 := models.NewUser()
+	user4.Username = "user4"
+	user4.Password = "hashed.password.4"
+	user4.Email = "user4@user.com"
+	user4.Name = "Fourth"
+	user4.Lastname = "User"
+
+	users[0].Username = "new-username"
+	users[0].Name = "New Name"
+	users[0].Lastname = "New Lastname"
+	users[0].Validated = true
+
+	tests := []struct {
+		name string
+		user *models.User
+		err  error
+	}{{
+		"non existing user",
+		user4,
+		ErrRepositoryUpdate,
+	}, {
+		"existing user",
+		users[0],
+		nil,
+	}}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert := assert.New(t)
+			r := NewRepository(db)
+
+			err := r.Update(test.user)
+
+			if test.err != nil {
+				errors.Assert(t, test.err, err)
+			} else {
+				assert.Nil(err)
+				row := db.QueryRow("SELECT id, username, name, lastname, validated, updated_at FROM users WHERE id = $1", test.user.ID)
+				var id, username, name, lastname string
+				var validated bool
+				var updatedAt time.Time
+				row.Scan(&id, &username, &name, &lastname, &validated, &updatedAt)
+				assert.Equal(test.user.ID, id)
+				assert.Equal(test.user.Username, username)
+				assert.Equal(test.user.Name, name)
+				assert.Equal(test.user.Lastname, lastname)
+				assert.Equal(test.user.Validated, validated)
+				assert.NotEmpty(updatedAt)
+			}
+		})
+	}
+}
+
+func TestRepositoryDelete(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	db, users := populate()
+
+	tests := []struct {
+		name string
+		id   string
+		err  error
+	}{{
+		"invalid id",
+		"user123",
+		ErrRepositoryDelete,
+	}, {
+		"non existing user",
+		models.NewID(),
+		ErrRepositoryDelete,
+	}, {
+		"existing user",
+		users[0].ID,
+		nil,
+	}}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert := assert.New(t)
+			r := NewRepository(db)
+
+			err := r.Delete(test.id)
+
+			if test.err != nil {
+				errors.Assert(t, test.err, err)
+			} else {
+				row := db.QueryRow("SELECT enabled, deleted_at FROM users WHERE id = $1", test.id)
+				var enabled bool
+				var deletedAt time.Time
+				row.Scan(&enabled, &deletedAt)
+				assert.Equal(false, enabled)
+				assert.NotEmpty(deletedAt)
 			}
 		})
 	}
